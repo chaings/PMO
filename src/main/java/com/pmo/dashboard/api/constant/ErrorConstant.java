013ca9d7726479a64b74ef6eeba0f38da737b8ed
zhangcn <zhangcn@yimidida.com>
2018-09-06 11:00:50 +0800

    myself test2

--- src/main/java/com/pmo/dashboard/api/constant/ErrorConstant.java
+++ src/main/java/com/pmo/dashboard/api/constant/ErrorConstant.java
@@ -3,7 +3,8 @@ package com.pmo.dashboard.api.constant;
 public class ErrorConstant {
 	
 	// hahahaa
-	
+	// hahahaa
+
 	public static final String CertificationFail = "00010";//认证失败
 	public static final String Exception = "00001";//系统异常
 
